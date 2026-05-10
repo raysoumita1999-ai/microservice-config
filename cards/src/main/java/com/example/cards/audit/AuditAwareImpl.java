b7f64ff191b642860c47d3decee917c41c790b13
Soumita <raysoumita1999@gmail.com>
2026-05-10 12:32:33 +0530

    Initial commit

---
+++ cards/src/main/java/com/example/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,17 @@
+package com.example.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware {
+
+
+
+    @Override
+    public Optional getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+}
